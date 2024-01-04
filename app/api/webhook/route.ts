01d381fe007a0e29d8d9ecbb9429ee4625b51132
Mark Barros <madae.the.ill.poet@gmail.com>
2024-01-04 15:22:26 -0800

    random change - line addition

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -15,6 +15,7 @@ export async function POST(req: Request) {
     );
   }
 
+  // random change
   // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
